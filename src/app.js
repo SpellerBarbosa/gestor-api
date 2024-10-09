701e407ed8346a5efd0a5d9e074ad0a492cceb2a
SpellerBarbosa <133154923+SpellerBarbosa@users.noreply.github.com>
2024-10-08 22:34:17 -0300

    update app.js, middleware cors()

--- src/app.js
+++ src/app.js
@@ -1,33 +1,38 @@
 import express from "express";
-import cors from 'cors';
+import cors from "cors";
 import connectToDB from "./config/connectToDB.js";
 import router from "./routes/routes.js";
 
-class App{
-    constructor(){
-        this.server = express();
-        this.port = process.env.PORT || 3001;
-        connectToDB();
+class App {
+  constructor() {
+    this.server = express();
+    this.port = process.env.PORT || 3001;
+    connectToDB();
 
-        this.middlewares();
-        this.routes();
+    this.middlewares();
+    this.routes();
+  }
 
-    }
+  middlewares() {
+    this.server.use(express.json());
+    this.server.use(
+      cors({
+        origin: "https://projeto-e-gestor.vercel.app", 
+        methods: ["GET", "POST"], 
+        credentials: true,
+      })
+    );
+  }
 
-    middlewares(){
-        this.server.use(express.json());
-        this.server.use(cors());
-    }
+  routes() {
+    this.server.use(router);
+  }
 
-    routes(){
-        this.server.use(router);
-    }
-
-    start(){
-        this.server.listen(this.port,()=>{
-            console.log(`http://localhost:${this.port}/api`);
-        });
-    }
+  start() {
+    this.server.listen(this.port, () => {
+      console.log(`http://localhost:${this.port}/api`);
+    });
+  }
 }
 
 export default App;
